ca2c4aa32ab04ca4db49907fe27aad9b787b353a
Scott <scottmcgowan@email.arizona.edu>
2013-07-31 22:54:45 -0700

    Test JBox commit

--- Doodle/src/gui/MainPanel.java
+++ Doodle/src/gui/MainPanel.java
@@ -4,11 +4,21 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+
+
+
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel {
 
+	
 	public MainPanel() {
 		super();
 		this.setPreferredSize(new Dimension(500, 500));
+		
+		Vec2 gravity = new Vec2(0.0f, -10.0f);
+		boolean doSleep = true;
+		World world = new World(gravity);
 	}
 }
