75b59c3def56a116893582559216573b35d2c942
Logan Barnes <barneslt63@gmail.com>
2013-07-31 20:36:58 -0700

    Initial commit

---
+++ Doodle/src/gui/GameApp.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.JApplet;
+import javax.swing.SwingUtilities;
+
+@SuppressWarnings("serial")
+public class GameApp extends JApplet {
+
+	@Override
+	public void init() {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				setContentPane(new MainPanel());
+				setSize(501, 501);
+			}
+		});
+	}
+}
---
+++ Doodle/src/gui/MainPanel.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class MainPanel extends JPanel {
+
+	public MainPanel() {
+		super();
+	}
+}
