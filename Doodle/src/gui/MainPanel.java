b89de4b66040de5609458d75e69e68db6f89bad4
Logan Barnes <barneslt63@gmail.com>
2013-07-31 20:56:06 -0700

    Test

--- Doodle/src/gui/MainPanel.java
+++ Doodle/src/gui/MainPanel.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -7,5 +9,6 @@ public class MainPanel extends JPanel {
 
 	public MainPanel() {
 		super();
+		this.setPreferredSize(new Dimension(500, 500));
 	}
 }
