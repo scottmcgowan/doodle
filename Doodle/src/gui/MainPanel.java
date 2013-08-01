27f33d9f5f0c2a0cfe4919b3800f30ec5d39cb12
Logan Barnes <barneslt63@gmail.com>
2013-07-31 22:53:14 -0700

    Working JBox2D Lib

--- Doodle/src/gui/MainPanel.java
+++ Doodle/src/gui/MainPanel.java
@@ -4,9 +4,17 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.World;
+
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel {
 
+	Vec2 vec = new Vec2(0.0f, -10.0f);
+	World world = new World(vec);
+	BodyDef ground = new BodyDef();
+	
 	public MainPanel() {
 		super();
 		this.setPreferredSize(new Dimension(500, 500));
